67c86e91f3fe83768bd88adfc78c7b3892e20a6a
zoubia127 <161927206+zoubia127@users.noreply.github.com>
2024-11-18 18:32:02 +0530

    SeasonGame.py

---
+++ Visaa/SeasonGame.py
@@ -0,0 +1,27 @@
+n=int(input())
+if n==1:
+    print("Winter")
+elif n==2:
+    print("Winter")
+elif n==3:
+    print("Spring")
+elif n==4:
+    print("Spring")
+elif n==5:
+    print("Spring")
+elif n==6:
+    print("Summer")
+elif n==7:
+    print("Summer")
+elif n==8:
+    print("Summer")
+elif n==9:
+    print("Autumn")
+elif n==10:
+    print("Autumn")
+elif n==11:
+    print("Autumn")
+elif n==12:
+    print("Winter")
+else:
+    print("Invalid")
